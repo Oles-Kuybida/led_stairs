4925cf42f6869da33485b1767a0943b3cb90ec96
OlesK <oleskuybida@gmail.com>
2018-11-12 03:51:03 +0200

    error reaction

--- Stairs_Light/Stairs_Light.ino
+++ Stairs_Light/Stairs_Light.ino
@@ -13,49 +13,34 @@
 
 #define LIGHT_REGISTOR_PIN A1
 
-#define DISP_CLK 5
-#define DISP_DIO 4
-
 #define UP_SONIC_TRIG_PIN 9
 #define UP_SONIC_ECHO_PIN 10
 
 #define DW_SONIC_TRIG_PIN 11
 #define DW_SONIC_ECHO_PIN 12
 
-#define ECODE_CNT_CLK_PIN 1 //ITEM_COUNT
-#define ECODE_CNT_DT_PIN 0
-boolean DT_CNT_now;
-boolean DT_CNT_last;
-long counter_CNT = 10;
+int ITEM_COUNT = 10;
+long STEP_DURATION = 500;//(miliseconds per step)
+int MAX_BRIGHT = 255;//0-255
+int WAIT_BRIGHT = 1;//0-255
 
-#define ECODE_LS_CLK_PIN 3 //LIGHT_SPEED (miliseconds per step)
-#define ECODE_LS_DT_PIN 2
-boolean DT_LS_now;
-boolean DT_LS_last;
-long counter_LS = 500;
-
-#define ECODE_MB_CLK_PIN 8 //MAX_BRIGHT;
-#define ECODE_MB_DT_PIN 7
-boolean DT_MB_now;
-boolean DT_MB_last;
-long counter_MB = 255;//0-255
-
-#define ECODE_WB_CLK_PIN 17 //WAIT_BRIGHT;
-#define ECODE_WB_DT_PIN 16
-boolean DT_WB_now;
-boolean DT_WB_last;
-long counter_WB = 1;//0-255
-
-long settingDisplayData;
-
-#define LED_DATA_PIN 6
+#define LED_DATA_PIN 8
 
 //modes
+#define TEST_MODE 10
+#define GO_MODE 20
+
 #define WAITING_MODE 0
-#define TEST_MODE 1
-#define GO_MODE 2
-#define SETTING_MODE 3
-#define SLEEP_MODE 4
+#define SETTING_ITEM_COUNT 1
+#define SETTING_STEP_DURATION 2
+#define SETTING_MAX_BRIGHT 3
+#define SETTING_WAIT_BRIGHT 4
+#define SLEEP_MODE 5
+
+int test_mode_value;//-1==OFF
+#define ALL_TEST = 0;
+#define UP_TEST = 1;
+#define DOWN_TEST = 2;
 
 //global variables
 int MODE;
@@ -79,16 +64,13 @@ CRGB leds[MAX_ITEM_COUNT ];//max count
 // reset pin not used on 4-pin OLED module
 Adafruit_SSD1306 display(-1);  // -1 = no reset pin
 
-bool isSettingActive = false;
-bool isDLTchanged = false;
-bool isLSchanged = false;
-bool isMBchanged = false;
-bool isWBchanged = false;
-
 bool isGoUp = false;
 bool isGoDw = false;
 
 int softLightPersent = 30;
+
+String errorText = "no error";
+int errorCode = 0;
 
 void printOnDisplay(String text, int number = -1)
 {
@@ -98,16 +80,16 @@ void printOnDisplay(String text, int number = -1)
   display.drawPixel(127, 0, WHITE);
   display.drawPixel(0, 31, WHITE);
   display.drawPixel(127, 31, WHITE);
-//
-//  display.setTextSize(1);
-//  display.setTextColor(WHITE);
-//  display.setCursor(27, 5);
-//  display.print(text);
-//  if (number != -1)
-//  {
-//    display.setCursor(27, 15);
-//    display.print(number);
-//  }
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(27, 5);
+  display.print(text);
+  if (number != -1)
+  {
+    display.setCursor(27, 15);
+    display.print(number);
+  }
 
   display.display();
 }
@@ -122,8 +104,7 @@ void setup()
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
   display.clearDisplay();
   display.display();
-  //disp.init();
-  //  disp.set(7);  //bright, 0 - 7 (min - max)
+
   //sonic
   pinMode(UP_SONIC_TRIG_PIN , OUTPUT);
   pinMode(UP_SONIC_ECHO_PIN , INPUT);
@@ -134,19 +115,6 @@ void setup()
   pinMode(MODE_BTN_PIN, INPUT);
   pinMode(PLUS_BTN_PIN, INPUT);
   pinMode(MINUS_BTN_PIN, INPUT);
-  //ECODE
-  pinMode (ECODE_CNT_CLK_PIN, INPUT);
-  pinMode (ECODE_CNT_DT_PIN, INPUT);
-  DT_CNT_last = digitalRead(ECODE_CNT_CLK_PIN);
-  pinMode (ECODE_LS_CLK_PIN, INPUT);
-  pinMode (ECODE_LS_DT_PIN, INPUT);
-  DT_LS_last = digitalRead(ECODE_LS_CLK_PIN);
-  pinMode (ECODE_MB_CLK_PIN, INPUT);
-  pinMode (ECODE_MB_DT_PIN, INPUT);
-  DT_MB_last = digitalRead(ECODE_MB_CLK_PIN);
-  pinMode (ECODE_WB_CLK_PIN, INPUT);
-  pinMode (ECODE_WB_DT_PIN, INPUT);
-  DT_WB_last = digitalRead(ECODE_WB_CLK_PIN);
 }
 
 void loop()
@@ -155,59 +123,57 @@ void loop()
   bool isMODEpressed = (digitalRead(MODE_BTN_PIN) == HIGH);
   bool isPLUSpressed = (digitalRead(PLUS_BTN_PIN) == HIGH);
   bool isMINUSpressed = (digitalRead(MINUS_BTN_PIN) == HIGH);
-  printOnDisplay("Hello, world!", isMODEpressed);
+  //printOnDisplay("Hello, world!", isMODEpressed);
 
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  if (isTESTpressed)
-  {
-    display.setCursor(5, 5);
-    display.print("T");
-  }
+  //  display.setTextSize(1);
+  //  display.setTextColor(WHITE);
+  //  if (isTESTpressed)
+  //  {
+  //    display.setCursor(5, 5);
+  //    display.print("T");
+  //  }
+  //
+  //  if (isMODEpressed)
+  //  {
+  //    display.setCursor(15, 5);
+  //    display.print("M");
+  //  }
+  //
+  //  if (isPLUSpressed)
+  //  {
+  //    display.setCursor(25, 5);
+  //    display.print("+");
+  //  }
+  //  if (isMINUSpressed)
+  //  {
+  //    display.setCursor(35, 5);
+  //    display.print("-");
+  //  }
+  //  display.display();
 
-  if (isMODEpressed)
-  {
-    display.setCursor(15, 5);
-    display.print("M");
-  }
-
-  if (isPLUSpressed)
-  {
-    display.setCursor(25, 5);
-    display.print("+");
-  }
-  if (isMINUSpressed)
-  {
-    display.setCursor(35, 5);
-    display.print("-");
-  }
-  display.display();
-
-
-  return;
   //###################### MODE DETERMINATION
   //TEST MODE DETECT
-  if (digitalRead(TEST_BTN_PIN) == HIGH)
+  if (isTESTpressed)
   {
     changeModeTo(TEST_MODE);
   }
   else
   {
     //SETTING MODE DETECT
-    isDLTchanged = encoderTick(ECODE_CNT_DT_PIN, ECODE_CNT_CLK_PIN, DT_CNT_now, DT_CNT_last, counter_CNT);
-    isLSchanged = encoderTick(ECODE_LS_DT_PIN, ECODE_LS_CLK_PIN, DT_LS_now, DT_LS_last, counter_LS);
-    isMBchanged = encoderTick(ECODE_MB_DT_PIN, ECODE_MB_CLK_PIN, DT_MB_now, DT_MB_last, counter_MB);
-    isWBchanged = encoderTick(ECODE_WB_DT_PIN, ECODE_WB_CLK_PIN, DT_WB_now, DT_WB_last, counter_WB);
-    if (false)//(isDLTchanged || isLSchanged || isMBchanged || isWBchanged)
+    if (isMODEpressed)
     {
-      changeModeTo(SETTING_MODE);
-      valideteSetting();
-      if (isDLTchanged) settingDisplayData = counter_CNT;
-      if (isLSchanged) settingDisplayData = counter_LS;
-      if (isMBchanged) settingDisplayData = counter_MB;
-      if (isWBchanged) settingDisplayData = counter_WB;
+      int newMode;
+      if ((MODE < 0) || (MODE > 5))
+        newMode = 0;
+      else
+        newMode = MODE + 1;
+      if (newMode > 5)
+        newMode = 0;
+
+      changeModeTo(newMode);
+      //valideteSetting();
     }
-    if ( MODE != SETTING_MODE)
+    if ( (MODE == WAITING_MODE) || (MODE == GO_MODE) )
     {
       if (isThereDayLight())
       {
@@ -217,13 +183,29 @@ void loop()
       {
         updateSonicDataUp();
         updateSonicDataDw();
-        if (!isGoUp)
+        UP_SONIC_DIST = 1; //TODO:
+        DW_SONIC_DIST = 45; //TODO:
+        //if no sonic
+        if (UP_SONIC_DIST == 0)
         {
-          isGoUp = UP_SONIC_DIST <= UP_S_TRIGGER;
+          errorCode = 1;
+          errorText = "NoUpSonic";
         }
-        if (!isGoDw)
+        if (DW_SONIC_DIST == 0)
         {
-          isGoDw = DW_SONIC_DIST <= DW_S_TRIGGER;
+          errorCode = 2;
+          errorText = "NoDownSonic";
+        }
+        if (errorCode == 0)
+        {
+          if (!isGoUp)
+          {
+            isGoUp = UP_SONIC_DIST <= UP_S_TRIGGER;
+          }
+          if (!isGoDw)
+          {
+            isGoDw = DW_SONIC_DIST <= DW_S_TRIGGER;
+          }
         }
         if (MODE != GO_MODE)
         {
@@ -244,63 +226,52 @@ void loop()
   //*************************************************
   delay(delayTime);
 
-  if (MODE == SETTING_MODE)
-  {
-    timerSett += delayTime;
-    if (timerSett > settinDelay)
-    {
-      timerSett = 0;
-      changeModeTo(WAITING_MODE);
-    }
-  }
-  else
-  {
-    if (isGoUp)
-      timerUp += delayTime;
-    if (isGoDw)
-      timerDw += delayTime;
-    const long fullTime = counter_CNT * counter_LS;
+  if (isGoUp)
+    timerUp += delayTime;
+  if (isGoDw)
+    timerDw += delayTime;
+  const long fullTime = ITEM_COUNT * STEP_DURATION;
 
-    if ((timerUp >= fullTime) || !isGoUp)
-    {
-      isGoUp = false;
-      timerUp = 0;
-    }
-    if ((timerDw >= fullTime) || !isGoDw)
-    {
-      isGoDw = false;
-      timerDw = 0;
-    }
-    if (MODE == GO_MODE && !isGoDw && !isGoUp)
-      changeModeTo(WAITING_MODE);
+  if ((timerUp >= fullTime) || !isGoUp)
+  {
+    isGoUp = false;
+    timerUp = 0;
   }
+  if ((timerDw >= fullTime) || !isGoDw)
+  {
+    isGoDw = false;
+    timerDw = 0;
+  }
+  if (MODE == GO_MODE && !isGoDw && !isGoUp)
+    changeModeTo(WAITING_MODE);
 }
 
 bool valideteSetting()
 {
-  if (counter_CNT < 2)
-    counter_CNT = 2;
-  if (counter_CNT > MAX_ITEM_COUNT)
-    counter_CNT = MAX_ITEM_COUNT;
+  if (ITEM_COUNT < 2)
+    ITEM_COUNT = 2;
+  if (ITEM_COUNT > MAX_ITEM_COUNT)
+    ITEM_COUNT = MAX_ITEM_COUNT;
 
-  if (counter_LS < 100)
-    counter_LS = 100;
-  if (counter_LS > 10000)
-    counter_LS = 10000;
+  if (STEP_DURATION < 100)
+    STEP_DURATION = 100;
+  if (STEP_DURATION > 10000)
+    STEP_DURATION = 10000;
 
-  if (counter_MB < 1)
-    counter_MB = 1;
-  if (counter_MB > 255)
-    counter_MB = 255;
+  if (MAX_BRIGHT < 1)
+    MAX_BRIGHT = 1;
+  if (MAX_BRIGHT > 255)
+    MAX_BRIGHT = 255;
 
-  if (counter_WB < 1)
-    counter_WB = 1;
-  if (counter_WB > 255)
-    counter_WB = 255;
+  if (WAIT_BRIGHT < 1)
+    WAIT_BRIGHT = 1;
+  if (WAIT_BRIGHT > 255)
+    WAIT_BRIGHT = 255;
 }
 
 bool isThereDayLight()
 {
+  return false;
   //check light sensor if is day or night now
   Serial.println(analogRead(LIGHT_REGISTOR_PIN));
   if (analogRead(LIGHT_REGISTOR_PIN) < 500)
@@ -313,6 +284,8 @@ void changeModeTo(int mode)
   timerUp = 0;
   timerDw = 0;
   MODE = mode;
+  Serial.print("Mode changed: ");
+  Serial.println(MODE);
 }
 
 void updateSonicDataUp()
@@ -328,8 +301,8 @@ void updateSonicDataUp()
   duration = pulseIn(UP_SONIC_ECHO_PIN , HIGH);
   UP_SONIC_DIST = (duration * .0343) / 2;
 
-  //  Serial.print("Distance up: ");
-  //  Serial.println(UP_SONIC_DIST );
+  Serial.print("Distance up: ");
+  Serial.println(UP_SONIC_DIST );
 }
 
 void updateSonicDataDw()
@@ -345,22 +318,25 @@ void updateSonicDataDw()
   duration = pulseIn(DW_SONIC_ECHO_PIN , HIGH);
   DW_SONIC_DIST = (duration * .0343) / 2; //meter
 
-  //  Serial.print("Distance: ");
-  //  Serial.print(DW_SONIC_DIST );
+  Serial.print("Distance dw: ");
+  Serial.print(DW_SONIC_DIST );
 }
 
 void displayData()
 {
+  if (errorCode != 0)
+  {
+    printOnDisplay(errorText);
+    return;
+  }
   switch (MODE)
   {
     case WAITING_MODE:
       printOnDisplay("Wait for move");
-      // disp.displayByte(_U, _A, _l, _t);
       brightWait();
       break;
     case TEST_MODE:
       printOnDisplay("Test mode");
-      //disp.displayByte(_t, _E, _S, _t);
       brightAll();
       break;
     case GO_MODE:
@@ -380,13 +356,20 @@ void displayData()
         bright();
       }
       break;
-    case SETTING_MODE:
-      printOnDisplay("Setting");
-      // disp.displayIntZero(settingDisplayData);//mode
+    case SETTING_ITEM_COUNT:
+      printOnDisplay("Setting ITEM COUNT");
+      break;
+    case SETTING_STEP_DURATION:
+      printOnDisplay("Setting STEP DURATION");
+      break;
+    case SETTING_MAX_BRIGHT:
+      printOnDisplay("Setting MAX BRIGHT");
+      break;
+    case SETTING_WAIT_BRIGHT:
+      printOnDisplay("Setting WAIT BRIGHT");
       break;
     case SLEEP_MODE:
       printOnDisplay("Sleep");
-      // disp.displayByte(_S, _L, _E, _P);
       shutDownAllPins();
       FastLED.show();
       break;
@@ -396,16 +379,16 @@ void displayData()
 void brightAll()
 {
   shutDownAllPins();
-  for (int i = 0; i < counter_CNT; ++i)
-    leds[i] = CRGB(counter_MB, counter_MB, counter_MB);
+  for (int i = 0; i < ITEM_COUNT; ++i)
+    leds[i] = CRGB(MAX_BRIGHT, MAX_BRIGHT, MAX_BRIGHT);
   FastLED.show();
 }
 
 void brightWait()
 {
   shutDownAllPins();
-  leds[0] =             CRGB(counter_WB, counter_WB, counter_WB);
-  leds[counter_CNT - 1] = CRGB(counter_WB, counter_WB, counter_WB);
+  leds[0] =             CRGB(WAIT_BRIGHT, WAIT_BRIGHT, WAIT_BRIGHT);
+  leds[ITEM_COUNT - 1] = CRGB(WAIT_BRIGHT, WAIT_BRIGHT, WAIT_BRIGHT);
   FastLED.show();
 }
 
@@ -431,15 +414,15 @@ void bright()
   shutDownAllPins();
   if (isGoUp)
   {
-    int _stepUp = timerUp / (counter_LS * index);
-    if (_stepUp < counter_CNT)
-      leds[_stepUp] = softLight(_stepUp * counter_LS * index, (counter_LS * index), timerUp, counter_MB);
+    int _stepUp = timerUp / (STEP_DURATION * index);
+    if (_stepUp < ITEM_COUNT)
+      leds[_stepUp] = softLight(_stepUp * STEP_DURATION * index, (STEP_DURATION * index), timerUp, MAX_BRIGHT);
   }
   if (isGoDw)
   {
-    int _stepDw = counter_CNT - 1 - timerDw / (counter_LS * index);
-    if (_stepDw < counter_CNT)
-      leds[_stepDw] = softLight(timerDw / (counter_LS * index) * counter_LS * index, (counter_LS * index), timerDw, counter_MB);
+    int _stepDw = ITEM_COUNT - 1 - timerDw / (STEP_DURATION * index);
+    if (_stepDw < ITEM_COUNT)
+      leds[_stepDw] = softLight(timerDw / (STEP_DURATION * index) * STEP_DURATION * index, (STEP_DURATION * index), timerDw, MAX_BRIGHT);
   }
   FastLED.show();
 }
@@ -449,23 +432,4 @@ void shutDownAllPins()
   for (int i = 0; i < MAX_ITEM_COUNT; ++i)
     leds[i] = CRGB(0, 0, 0);
   //need to show() for apply
-}
-
-bool encoderTick(const int DT, const int CLK, boolean & DT_now, boolean & DT_last, long & counter)
-{
-  DT_now = digitalRead(CLK);
-  if (DT_now != DT_last)
-  {
-    if (digitalRead(DT) != DT_now)
-    {
-      counter ++;
-    }
-    else
-    {
-      counter --;
-    }
-  }
-  bool res = (DT_last != DT_now);
-  DT_last = DT_now;
-  return res;
 }
